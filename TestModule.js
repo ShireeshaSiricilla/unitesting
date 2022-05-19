4daa85e254211ff1e05f337390b3a129eb9f0928
ShireeshaSiricilla <103118040+ShireeshaSiricilla@users.noreply.github.com>
2022-05-19 22:28:55 +0530

    Add files via upload

---
+++ TestModule.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const request = require('request')
+
+function saveWebpage (url, filePath) {
+  return getWebpage(url, filePath)
+    .then(writeFile)
+}
+
+function getWebpage (url) {
+  return new Promise (function (resolve, reject) {
+    request.get(url, function (err, response, body) {
+      if (err) {
+        return reject(err)
+      }
+
+      resolve(body)
+      console.log("unit-test pass")
+    })
+  })
+}
+
+function writeFile (fileContent) {
+  let filePath = 'page'
+  return new Promise (function (resolve, reject) {
+    fs.writeFile(filePath, fileContent, function (err) {
+      if (err) {
+        return reject(err)
+      }
+
+      resolve(filePath)
+      console.log("unit-test pass")
+    })
+  })
+}
+
+module.exports = {
+  saveWebpage
+}
